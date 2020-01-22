1bdef847083f2b9264e5b4ea8c76cf2c659019eb
Ali Ahsan <info.aliuetian@gmail.com>
2020-01-22 05:55:21 +0500

    Added gradle support for dataBinding, lifecycle, vectorDrawables, cardView & revamped gradle structure

--- app/src/main/java/com/yourappsgeek/adventcalender/MainActivity.kt
+++ app/src/main/java/com/yourappsgeek/adventcalender/MainActivity.kt
@@ -2,11 +2,20 @@ package com.yourappsgeek.adventcalender
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.yourappsgeek.adventcalender.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        /*
+           The layout for this activity is a Data Binding layout so it needs to be inflated using
+           DataBindingUtil.
+        */
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
     }
 }
